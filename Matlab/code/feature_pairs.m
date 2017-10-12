9bca4fee3b46dfa039c5b0dc831b1d1fc0d39870
Benjamin James <benjamin-james@utulsa.edu>
2017-10-12 17:36:36 -0500

    added README and added bugfix for paired features

--- Matlab/code/feature_pairs.m
+++ Matlab/code/feature_pairs.m
@@ -2,16 +2,28 @@ function newFeatures = feature_pairs(Features)
 %   FEATURE_PAIRS computes the multiplicative statistical pairs
 %
 %
-[r, c] = size(Features);
-newFeatures=zeros(r, c * (c-1) / 2);
+% [r, c] = size(Features);
+% newFeatures=zeros(r, c * (c-1) / 2);
+% 
+% for n=1:c
+%     f1=Features(:,n);
+%     for m=n+1:c
+%         f2=Features(:,m);
+%         newFeatures(:,n) = f1.*f2;
+%     end
+% end
+% 
+% end
 
-for n=1:c
+newFeatures=[];
+stop=size(Features,2);
+for n=1:stop
     f1=Features(:,n);
-    for m=n+1:c
+    for m=n+1:stop
         f2=Features(:,m);
-        newFeatures(:,n) = f1.*f2;
+        result=f1.*f2;
+        newFeatures=[newFeatures result];
     end
 end
 
 end
-
